d4d07813bba3dee80d91b2ec246b3a5e4b540c73
omarfrikha96 <omar.frikha96@gmail.com>
2023-07-11 22:25:49 +0100

    add an alerte with js

--- Week3/Day4/dojoweather/script.js
+++ Week3/Day4/dojoweather/script.js
@@ -3,3 +3,9 @@ const banner = document.querySelector(".cookies-banner");
 const accept = () => {
     banner.classList.add("hide-cookies-banner")
 };
+
+
+const loading = () => {
+    alert("Loading weather report....")
+
+}
